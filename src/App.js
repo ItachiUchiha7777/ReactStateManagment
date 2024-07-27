deeca01d4f2ae36a6de4bddec1ced44b6e282a9d
Rohit Gusain <rohitgusain792@gmail.com>
2024-07-27 17:12:28 +0530

    use state list

--- src/App.js
+++ src/App.js
@@ -2,17 +2,61 @@
 import './App.css';
 import {useState} from "react";
 
+
+
+function Namelist(){
+    let [list,setList]=useState(["Rohit","Mohit","Shankara"])
+    const [name,setName]=useState("");
+    const onAddName=()=>{
+
+        setList([...list,name])
+        setName("")
+    }
+    return (
+        <div>
+            <ul>
+                {list.map((name) => {
+                    return <li key={name}>{name}</li>;
+                })}
+            </ul>
+            <input type="text" value={name}
+            onChange={(e)=>
+                setName(e.target.value)}/>
+        <button onClick={onAddName}>Add Name</button>
+        </div>
+
+    )
+}
+
+
+
+function Counter(){
+    const [count, setCount] = useState(0)
+    function addOne(){
+        setCount(count+1)
+        console.log(count)
+    }
+    return (
+        <div>
+            <button onClick={addOne}>Press me {count}</button>
+
+        </div>
+    );
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
-  function addOne(){
-    setCount(count+1)
-    console.log(count)
-  }
+
   return (
     <div className="App">
       <header className="App-header">
-  <counter/>
-        <p> The value of count is {count}</p>
+        <Namelist/>
+            <Counter/>
+          <Counter/>
+          <Counter/>
+          <Counter/>
+
+
       </header>
     </div>
   );
